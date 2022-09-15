9b20c3a8f584bffbc7891b7cd43e2a91ab1e1b6d
Thales Alves <thalesalves758@gmail.com>
2022-09-15 16:46:55 -0300

    feat: import and use index router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import router from './routes/index.routes';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 export default app;
