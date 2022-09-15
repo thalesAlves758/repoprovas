9519c89a4b59723d192572407b8330e4d15d4005
Thales Alves <thalesalves758@gmail.com>
2022-09-15 15:19:24 -0300

    test: create sign up route tests

---
+++ tests/Auth/signUp.test.ts
@@ -0,0 +1,42 @@
+import { prisma } from '../../src/config/database';
+import { generateSignUpBody } from '../factories/auth.factory';
+import app from '../../src/app';
+import request from 'supertest';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+describe('### POST /sign-up ###', () => {
+  beforeEach(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY`;
+  });
+
+  it('should return 200 status code', async () => {
+    const signUpBody = generateSignUpBody();
+    
+    const response = await request(app).post('/sign-up').send(signUpBody);
+
+    expect(response.status).toEqual(201);
+  });
+
+  it('should return 409 status code', async () => {
+    const signUpBody = generateSignUpBody();
+
+    await request(app).post('/sign-up').send(signUpBody);
+    const response = await request(app).post('/sign-up').send(signUpBody);
+
+    expect(response.status).toEqual(409);
+  });
+
+  it('should return 422 status code', async () => {
+    const signUpBody = {};
+
+    const response = await request(app).post('/sign-up').send(signUpBody);
+
+    expect(response.status).toEqual(422);
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  })
+});
