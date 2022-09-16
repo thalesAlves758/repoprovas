83e03e9061ea6db6f52a0fb8f15aa154b893ce78
Thales Alves <thalesalves758@gmail.com>
2022-09-16 15:42:06 -0300

    feat: create sign in request handler

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
 import { HttpStatus } from '../types/http.types';
-import { SignUpBodyData } from '../types/auth.types';
+import {
+  SignInBodyData,
+  SignInResponse,
+  SignUpBodyData,
+} from '../types/auth.types';
 import * as authServices from '../services/auth.services';
 
 export async function signUp(req: Request, res: Response) {
@@ -9,4 +13,15 @@ export async function signUp(req: Request, res: Response) {
   await authServices.signUp({ email, password });
 
   res.sendStatus(HttpStatus.CREATED);
+}
+
+export async function signIn(req: Request, res: Response) {
+  const { email, password }: SignInBodyData = req.body;
+
+  const response: SignInResponse = await authServices.signIn({
+    email,
+    password,
+  });
+
+  res.send(response);
 }
