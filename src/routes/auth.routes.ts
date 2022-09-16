b1d12dad48a364d6e34040fa26e4911fbbfe3f70
Thales Alves <thalesalves758@gmail.com>
2022-09-16 15:41:45 -0300

    feat: create POST /sign-in route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import * as authController from '../controllers/auth.controller';
 import { validateSchema } from '../middlewares/validateSchema.middleware';
+import signInSchema from '../schemas/signIn.schema';
 import signUpSchema from '../schemas/signUp.schema';
 
 const authRouter: Router = Router();
@@ -9,6 +10,11 @@ authRouter.post(
   '/sign-up',
   validateSchema(signUpSchema),
   authController.signUp
+);
+authRouter.post(
+  '/sign-in',
+  validateSchema(signInSchema),
+  authController.signIn
 );
 
 export default authRouter;
