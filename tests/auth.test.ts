2d86f6b363ea31f315b5f19c333ca8608748c080
Thales Alves <thalesalves758@gmail.com>
2022-09-16 15:16:08 -0300

    test: create sign in tests

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,13 +1,18 @@
 import { prisma } from '../src/config/database';
-import { generateSignUpBody } from './factories/auth.factory';
+import {
+  generateSignUpBody,
+  generateSignInBody,
+} from './factories/auth.factory';
 import app from '../src/app';
 import request from 'supertest';
+import { insertRandomUser } from './utils/user.utils';
+import { faker } from '@faker-js/faker';
+
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY`;
+});
 
 describe('### POST /sign-up ###', () => {
-  beforeEach(async () => {
-    await prisma.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY`;
-  });
-
   it('should return 200 status code', async () => {
     const signUpBody = generateSignUpBody();
 
@@ -34,8 +39,46 @@ describe('### POST /sign-up ###', () => {
     expect(response.status).toEqual(422);
     expect(response.text).toContain('Validation error:');
   });
+});
 
-  afterAll(async () => {
-    await prisma.$disconnect();
+describe('### POST /sign-in ###', () => {
+  it('should return 200 status code and a token in the response', async () => {
+    const user = await insertRandomUser();
+
+    const response = await request(app).post('/sign-in').send(user);
+
+    expect(response.status).toEqual(200);
+    expect(response.body).toMatchObject({ token: '' });
   });
+
+  it('should return 422 status code and a validation error message', async () => {
+    const response = await request(app).post('/sign-in').send({});
+
+    expect(response.status).toEqual(422);
+    expect(response.text).toContain('Validation error:');
+  });
+
+  it('should return 401 status code and an error message when send a wrong email', async () => {
+    const user = generateSignInBody();
+
+    const response = await request(app).post('/sign-in').send(user);
+
+    expect(response.status).toEqual(401);
+    expect(response.text).toEqual('Invalid email or password');
+  });
+
+  it('should return 401 status code and an error message when send a wrong password', async () => {
+    const { email } = await insertRandomUser();
+
+    const response = await request(app)
+      .post('/sign-in')
+      .send({ email, password: faker.internet.password() });
+
+    expect(response.status).toEqual(401);
+    expect(response.text).toEqual('Invalid email or password');
+  });
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
 });
