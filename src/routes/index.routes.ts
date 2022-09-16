b4a31a7fe3af29ee96ae1a2b140300921e5d3063
Thales Alves <thalesalves758@gmail.com>
2022-09-16 20:58:39 -0300

    feat: import and use tests router

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRouter from './auth.routes';
+import testRouter from './test.routes';
 
 const router: Router = Router();
 
 router.use(authRouter);
+router.use(testRouter);
 
 export default router;
