38800d05f547593eda8d7289584e1ffa18745f44
Thales Alves <thalesalves758@gmail.com>
2022-09-18 15:49:06 -0300

    test: rename entity folder name

--- tests/Test/getTestByDiscipline.test.ts
+++ tests/Test/getTestByDiscipline.test.ts
@@ -1,20 +1,33 @@
 import { faker } from '@faker-js/faker';
-import supertest from 'supertest';
 import request from 'supertest';
 import app from '../../src/app';
+import { prisma } from '../../src/config/database';
 import { authenticateRandomUser } from '../utils/auth.utils';
 import { insertRandomTest } from '../utils/test.utils';
 
+beforeEach(async () => {
+  await prisma.$executeRaw`
+    TRUNCATE TABLE
+      tests,
+      categories,
+      disciplines,
+      terms,
+      teachers,
+      "teachersDisciplines"
+    RESTART IDENTITY
+  `;
+});
+
 describe('### GET /tests/by/discipline ###', () => {
   it(`should return 401 status code and an error message when don't send token`, async () => {
-    const response = await supertest(app).get('/tests/by/discipline');
+    const response = await request(app).get('/tests/by/discipline');
 
     expect(response.status).toEqual(401);
     expect(response.text).toEqual('A token is required');
   });
 
   it(`should return 401 status code and an error message when send an invalid token`, async () => {
-    const response = await supertest(app)
+    const response = await request(app)
       .get('/tests/by/discipline')
       .set({ Authorization: `Bearer ${faker.random.word()}` });
 
@@ -28,7 +41,7 @@ describe('### GET /tests/by/discipline ###', () => {
       insertRandomTest(),
     ]);
 
-    const response = await supertest(app)
+    const response = await request(app)
       .get('/tests/by/discipline')
       .set({ Authorization: `Bearer ${token}` });
 
@@ -36,4 +49,8 @@ describe('### GET /tests/by/discipline ###', () => {
     expect(response.body).toBeInstanceOf(Array);
     expect(response.body).toHaveLength(1);
   });
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
 });
