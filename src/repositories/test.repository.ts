52bdcabda89485084daf3b3fa3d1116d5528ebc2
Thales Alves <thalesalves758@gmail.com>
2022-09-18 15:49:46 -0300

    feat: create a query to find tests grouped by disciplines

--- src/repositories/test.repository.ts
+++ src/repositories/test.repository.ts
@@ -1,7 +1,55 @@
 import { Test } from '@prisma/client';
 import { prisma } from '../config/database';
-import { TestInsertData } from '../types/test.types';
+import {
+  TestGroupedByDisciplineData,
+  TestInsertData,
+} from '../types/test.types';
 
 export async function insert(data: TestInsertData): Promise<Test> {
   return prisma.test.create({ data });
+}
+
+export async function findGroupedByDiscipline(): Promise<
+  TestGroupedByDisciplineData[]
+> {
+  return prisma.$queryRaw`
+    SELECT 
+      "terms".number AS "term",
+      ARRAY(
+        SELECT row_to_json("disciplineRow")
+        FROM (
+          SELECT
+            "disciplines".*,
+            ARRAY (
+              SELECT row_to_json("categoryRow")
+              FROM (
+                SELECT 
+                  "categories".*,
+                  ARRAY (
+                    SELECT row_to_json("testRow")
+                    FROM (
+                      SELECT
+                        "tests".*,
+                        "teachers".name AS "teacher"
+                      FROM "tests"
+                      JOIN "teachersDisciplines" ON "teachersDisciplines".id = "tests"."teacherDisciplineId"
+                      JOIN "teachers" ON "teachers".id = "teachersDisciplines"."teacherId"
+                      WHERE "tests"."categoryId" = "categories".id
+                    ) "testRow"
+                  ) AS "tests"
+                FROM categories
+                JOIN "tests" ON "tests"."categoryId" = "categories".id
+                JOIN "teachersDisciplines" ON "tests"."teacherDisciplineId" = "teachersDisciplines".id
+                WHERE "disciplines".id = "teachersDisciplines"."disciplineId"
+              ) "categoryRow"
+            ) AS "categories"
+          FROM "disciplines"
+          WHERE "disciplines"."termId" = "terms".id
+        ) "disciplineRow"
+      ) AS "disciplines"
+    FROM "terms"
+    LEFT JOIN "disciplines" ON "disciplines"."termId" = "terms".id
+    GROUP BY "terms".id
+    ORDER BY "terms".number
+  `;
 }
