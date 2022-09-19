3869467925c0c179a83f49433b954e4a885cb96b
Thales Alves <thalesalves758@gmail.com>
2022-09-19 15:42:40 -0300

    feat: create a query to get tests grouped by teachers

--- src/repositories/test.repository.ts
+++ src/repositories/test.repository.ts
@@ -2,6 +2,7 @@ import { Test } from '@prisma/client';
 import { prisma } from '../config/database';
 import {
   TestGroupedByDisciplineData,
+  TestGroupedByTeacherData,
   TestInsertData,
 } from '../types/test.types';
 
@@ -51,5 +52,45 @@ export async function findGroupedByDiscipline(): Promise<
     LEFT JOIN "disciplines" ON "disciplines"."termId" = "terms".id
     GROUP BY "terms".id
     ORDER BY "terms".number
+  `;
+}
+
+export async function findGroupedByTeacher(): Promise<
+  TestGroupedByTeacherData[]
+> {
+  return prisma.$queryRaw`
+    SELECT 
+      "outerTeachers".name AS "teacher",
+      ARRAY (
+        SELECT row_to_json("categoryRow")
+        FROM (
+          SELECT
+            "categories".*,
+            ARRAY (
+              SELECT row_to_json("testRow")
+              FROM (
+                SELECT 
+                  "tests".id,
+                  "tests".name,
+                  "tests"."pdfUrl",
+                  "tests"."categoryId",
+                  "disciplines".name AS "discipline"
+                FROM "tests"
+                JOIN "teachersDisciplines" ON "teachersDisciplines".id = "tests"."teacherDisciplineId"
+                JOIN "teachers" ON "teachers".id = "teachersDisciplines"."teacherId"
+                JOIN "disciplines" ON "disciplines".id = "teachersDisciplines"."disciplineId"
+                WHERE "teachers".id = "outerTeachers".id AND "tests"."categoryId" = "categories".id
+              ) "testRow"
+            ) AS "tests"
+          FROM "categories"
+          JOIN "tests" ON "tests"."categoryId" = "categories".id
+          JOIN "teachersDisciplines" ON "teachersDisciplines".id = "tests"."teacherDisciplineId"
+          JOIN "teachers" ON "teachersDisciplines"."teacherId" = "teachers".id
+          WHERE "teachers".id = "outerTeachers".id
+          GROUP BY "categories".id
+        ) "categoryRow"
+      ) AS "categories"
+    FROM "teachers" "outerTeachers"
+    GROUP BY "outerTeachers".id
   `;
 }
